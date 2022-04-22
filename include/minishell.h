b2deec743df4f37c5ff4784906af5a6eba4a9da2
Rob-Yoo <jin12243@naver.com>
2022-04-22 17:21:26 +0900

    Feat : Add Pipe, redirection Struct

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 02:13:40 by minjupar          #+#    #+#             */
-/*   Updated: 2022/04/20 14:54:22 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/04/22 17:20:42 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@
 # include <stdlib.h>
 # include <signal.h>
 # include <termios.h>
+# include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <sys/wait.h>
-# include <fcntl.h>
+# include <sys/stat.h>
 
 # include "../libft/libft.h"
 
@@ -33,6 +34,27 @@ typedef struct s_state
 	char	**envp;
 	int		exit_status;
 }	t_state;
+
+typedef struct s_redir {
+	int				cnt; //갯수
+	char			*file_name;
+	struct s_redir	*next;
+	struct s_redir	*prev;
+}	t_redir;
+
+/*
+	pipe_type: 0 : 시작, 1: 중간, 2: 끝, 3: 혼자있을 때
+*/
+typedef struct s_cmd {
+	int				pipe_type;
+	char			*cmd;
+	char			option;   //n
+	struct s_cmd	*next;
+	struct s_cmd	*prev;
+	struct t_redir	*input;
+	struct t_redir	*output;
+	char			*heredoc; //종료문자
+}	t_cmd;
 
 /*global*/
 t_state	g_state;

44a9928d10164be86df0a1c2ec0d550e33eeeebf
Rob-Yoo <jin12243@naver.com>
2022-04-25 17:04:13 +0900

    Feat : >> > < 추가

--- srcs/redirection/redirection.c
+++ srcs/redirection/redirection.c
@@ -6,35 +6,44 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 14:52:29 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/04/24 21:46:34 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/04/25 17:02:29 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	redirect_in(char *in) // <
+int	rdr_in(char *in)
 {
-	int	fd;
+	int fd;
+	int	r_fd;
 
+	r_fd = dup(READ);
 	fd = open(in, O_RDONLY);
 	dup2(fd, READ);
 	close(fd);
+	return (r_fd);
 }
 
-void	redirect_out(char *out) // >
+int	rdr_out(char *out, int flag)
 {
 	int		fd;
-	int		ret;
-	char	*line;
+	int		w_fd;
 
+	w_fd = dup(WRITE);
 	fd = open(out, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	dup2(fd, 1);
-	close(fd);
-	while ((ret = get_next_line(0, &line)) > 0)
-	{
-		printf("%s\n", line);
-		free(line);
-	}
-	printf("%s", line);
-	free(line);
+	dup2(fd, WRITE);
+	return (w_fd);
 }
+
+int	rdr_double_out(char *out, int flag)
+{
+	int		fd;
+	int		w_fd;
+
+	w_fd = dup(WRITE);
+	fd = open(out, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	dup2(fd, WRITE);
+	return (w_fd);
+}
+
+

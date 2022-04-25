1709e760b9c13e2780058f99158f3bceb2d8894f
Rob-Yoo <jin12243@naver.com>
2022-04-25 21:33:28 +0900

    Fix : redirection

--- srcs/redirection/redirection.c
+++ srcs/redirection/redirection.c
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 14:52:29 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/04/25 17:53:57 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/04/25 19:09:48 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,6 @@ int	rdr_l(char *in)
 	return (r_fd);
 }
 
-int	rdr_r(char *out, int flag)
-{
-	int		fd;
-	int		w_fd;
-
-	(void)flag;
-	w_fd = dup(WRITE);
-	fd = open(out, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	dup2(fd, WRITE);
-	return (w_fd);
-}
-
 int	rdr_rr(char *out, int flag)
 {
 	int		fd;
@@ -44,6 +32,42 @@ int	rdr_rr(char *out, int flag)
 	(void)flag;
 	w_fd = dup(WRITE);
 	fd = open(out, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	dup2(fd, WRITE);
+	return (w_fd);
+}
+
+int	rdr_ll(char *heredoc)
+{
+	int		fd;
+	char	*line;
+	int		r_fd;
+
+	r_fd = dup(READ);
+	fd = open("tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	while (get_next_line(0, &line) > 0)
+	{
+		if (ft_strncmp(line, heredoc, ft_strlen(heredoc)) == 0)
+			break;
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		free(line);
+	}
+	if (ft_strncmp(line, heredoc, ft_strlen(heredoc)) == 0)
+		free(line);
+	// dup2(fd, READ);
+	// close(fd);
+	// unlink("tmp");
+	// ctrl + D 처리되면 안됨
+	return (r_fd);
+}
+int	rdr_r(char *out, int flag)
+{
+	int		fd;
+	int		w_fd;
+
+	(void)flag;
+	w_fd = dup(WRITE);
+	fd = open(out, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	dup2(fd, WRITE);
 	return (w_fd);
 }

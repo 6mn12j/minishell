9c62c1b92b5ca71c29627fdce94e31d34c4525f6
Rob-Yoo <jin12243@naver.com>
2022-04-26 15:53:52 +0900

    Feat : 명령어 실행 및 리다이렉션 처리

--- srcs/execute.c
+++ srcs/execute.c
@@ -1,26 +1,88 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   excute.c                                           :+:      :+:    :+:   */
+/*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 17:07:58 by minjupar          #+#    #+#             */
-/*   Updated: 2022/04/25 22:40:13 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/04/26 15:49:28 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*get_valid_cmd(char *cmd, char *path)
+{
+	char		*tmp;
+	char		*cmd_path;
+	char		**env_paths;
+	int			i;
+	struct stat	buf;
+
+	i = -1;
+	env_paths = ft_split(path, ':');
+	while (env_paths[++i])
+	{
+		tmp = ft_strjoin(env_paths[i], "/");
+		cmd_path = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (stat(cmd_path, &buf) == 0)
+			return (cmd_path);
+	}
+	return (NULL);
+}
+
+int	exec_one_cmd(t_cmd *command)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	else if (!pid)
+	{
+		if (command->input)
+			rdr_l(command->input->file_name);
+		else if (command->output)
+		{
+			if (command->output->type == 1)
+				rdr_r(command->output);
+			else if (command->output->type == 2)
+				rdr_rr(command->output);
+		}
+		execve(command->cmd, command->argv, g_state.envp);
+	}
+	else
+		waitpid(pid, NULL, 0);
+	return (1);
+}
+
 void	execute_cmd(t_cmd *command)
 {
+	char	*path;
+	int		flag;
+
+	path = get_env("PATH");
 	if (!command->next && command->cmd)
 	{
 		//빌트인 실행
-		if (command->)
+
+		//  ---------빌트인이 아닌 명령어 실행------------
+		if (!command->is_path)
+		{
+			command->cmd = get_valid_cmd(command->cmd, path);
+			flag = 1;
+			// if (!command->cmd)
+			// 	error
+		}
+		exec_one_cmd(command);
+		// ----------------------------------------
 	}
 	else
 	{
 		//파이프 여러개 일때
 	}
+	if (flag)
+		free(command->cmd);
 }

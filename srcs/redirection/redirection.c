c508105600971de64ac4e9cf1811d83f60ce2239
Rob-Yoo <jin12243@naver.com>
2022-04-26 15:54:56 +0900

    Fix : Redirection

--- srcs/redirection/redirection.c
+++ srcs/redirection/redirection.c
@@ -6,7 +6,7 @@
 /*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 14:52:29 by jinyoo            #+#    #+#             */
-/*   Updated: 2022/04/25 22:40:10 by jinyoo           ###   ########.fr       */
+/*   Updated: 2022/04/26 15:09:24 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,23 @@ int	rdr_l(char *in)
 	return (r_fd);
 }
 
-int	rdr_rr(char *out, int flag)
+int	rdr_rr(t_redir *redir)
 {
-	int		fd;
-	int		w_fd;
+	int			fd;
+	int			w_fd;
+	t_redir		*rdr;
 
-	(void)flag;
 	w_fd = dup(WRITE);
-	fd = open(out, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	rdr = redir;
+	while (rdr->next)
+	{
+		fd = open(rdr->file_name, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		close(fd);
+		rdr = rdr->next;
+	}
+	fd = open(rdr->file_name, O_WRONLY | O_APPEND | O_CREAT, 0644);
 	dup2(fd, WRITE);
+	close(fd);
 	return (w_fd);
 }
 
@@ -41,12 +49,12 @@ int	rdr_ll(char *heredoc)
 	int		fd;
 	char	*line;
 	int		r_fd;
-	int		idx;
-	int		filename;
+	// int		idx;
+	// int		filename;
 
 	r_fd = dup(READ);
-	filename = "tmp" + "idx";
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	// filename = "tmp" + "idx";
+	fd = open("tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	while (get_next_line(0, &line) > 0)
 	{
 		if (ft_strncmp(line, heredoc, ft_strlen(heredoc)) == 0)
@@ -63,14 +71,23 @@ int	rdr_ll(char *heredoc)
 	// ctrl + D 처리되면 안됨
 	return (r_fd);
 }
-int	rdr_r(char *out, int flag)
+
+int	rdr_r(t_redir *redir)
 {
 	int		fd;
 	int		w_fd;
+	t_redir		*rdr;
 
-	(void)flag;
 	w_fd = dup(WRITE);
-	fd = open(out, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	rdr = redir;
+	while (rdr->next)
+	{
+		fd = open(rdr->file_name, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+		close(fd);
+		rdr = rdr->next;
+	}
+	fd = open(rdr->file_name, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	dup2(fd, WRITE);
+	close(fd);
 	return (w_fd);
 }

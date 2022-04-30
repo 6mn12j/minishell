345ad06a9fea11b2d09c30454b6b08f7372dbd3d
6mn12j <dbwk0404@hanmail.net>
2022-04-30 20:31:31 +0900

    Fix : 파이프 인자 값 없을때 세그폴트 수정
    ex) echo | |, cat | | 일때 세그폴트 수정

--- srcs/parse/set_is_path.c
+++ srcs/parse/set_is_path.c
@@ -6,7 +6,7 @@
 /*   By: minjupar <minjupar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 03:19:21 by minjupar          #+#    #+#             */
-/*   Updated: 2022/04/27 02:56:37 by minjupar         ###   ########.fr       */
+/*   Updated: 2022/04/30 20:18:41 by minjupar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	set_is_path(t_cmd *cmd)
 		return ;
 	while (cur)
 	{
+		if (!cur->cmd)
+			return ;
 		if (ft_strchr(cur->cmd, '/'))
 			cur->is_path = 1;
 		cur = cur->next;

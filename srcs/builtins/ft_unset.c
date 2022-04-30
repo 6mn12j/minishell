c48501f6949274b3111921a54bc9145d5fc3c791
6mn12j <dbwk0404@hanmail.net>
2022-05-01 03:12:05 +0900

    Feat : ft_unset 빌트인 추가

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: minjupar <minjupar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 21:32:44 by minjupar          #+#    #+#             */
-/*   Updated: 2022/04/30 21:42:00 by minjupar         ###   ########.fr       */
+/*   Updated: 2022/05/01 03:07:22 by minjupar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,29 @@
 
 void	ft_unset(t_cmd *command)
 {
-	(void)command;
-	printf("ft_unset\n");
-	return ;
+	int		i;
+	int		j;
+	int		index;
+	char	**temp;
+
+	if (!check_key(command->argv[1]))
+	{
+		printf("bash: unset: `%s': not a valid identifier\n",command->argv[1]);
+		return ;
+	}
+	index = get_env_index(command->argv[1]);
+	temp = (char **)malloc(sizeof(char *) * ft_twoptr_len(g_state.envp));
+	if (!temp)
+		return (ft_error());
+	i = -1;
+	j = -1;
+	while (g_state.envp[++i])
+	{
+		if (i != index)
+			temp[++j] = g_state.envp[i];
+	}
+	temp[j] = NULL;
+	free(g_state.envp[index]);
+	free(g_state.envp);
+	g_state.envp = temp;
 }

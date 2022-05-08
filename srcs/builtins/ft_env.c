686560b0cfa4142b481c97c8d959658b41bcfa1c
Rob-Yoo <jin12243@naver.com>
2022-05-08 17:26:22 +0900

    Fix : command 구조체 삭제

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -3,20 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjupar <minjupar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 21:32:44 by minjupar          #+#    #+#             */
-/*   Updated: 2022/05/02 18:45:59 by minjupar         ###   ########.fr       */
+/*   Updated: 2022/05/08 17:19:20 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_env(t_cmd *command)
+void	ft_env(void)
 {
 	int	i;
 
-	(void)command;
 	i = -1;
 	while (g_state.envp[++i])
 		printf("%s\n", g_state.envp[i]);

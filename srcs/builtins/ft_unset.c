a10487841a9ad7f5c142025ff02045aaf1ee9441
Rob-Yoo <jin12243@naver.com>
2022-05-08 17:27:13 +0900

    Fix : command 구조체 삭제

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjupar <minjupar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 21:32:44 by minjupar          #+#    #+#             */
-/*   Updated: 2022/05/02 18:47:18 by minjupar         ###   ########.fr       */
+/*   Updated: 2022/05/08 17:22:02 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_unset_error_printf(char *error_props)
 {
 	printf("bash: unset: `%s': not a valid identifier\n", error_props);
+	g_state.exit_status = 1;
 	return ;
 }
 

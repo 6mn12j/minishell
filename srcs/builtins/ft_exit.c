1bbd8b066e32397392310b7b713a2e207256adca
Rob-Yoo <jin12243@naver.com>
2022-05-08 17:27:01 +0900

    Fix : 오류 처리 밒 종료코드

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjupar <minjupar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 21:32:44 by minjupar          #+#    #+#             */
-/*   Updated: 2022/05/02 18:30:34 by minjupar         ###   ########.fr       */
+/*   Updated: 2022/05/08 17:16:44 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,22 @@ void	ft_exit(t_cmd *command)
 {
 	if (command->is_pipe)
 		return ;
-	if (command->argc > 2)
+	if (!command->argv[1])
 	{
-		printf("soobash: exit: too many arguments\n");
-		return ;
+		printf("exit\n");
+		exit(127);
 	}
-	if (!check_exit(command->argv[1]))
+	else if (!check_exit(command->argv[1]))
 	{
+		printf("exit\n");
 		printf("soobash: exit: %s", command->argv[1]);
 		printf(": numeric argument required\n");
+		exit(255);
+	}
+	else if (command->argc > 2)
+	{
+		printf("soobash: exit: too many arguments\n");
+		g_state.exit_status = 1;
 		return ;
 	}
-	g_state.exit_status = ft_atoi(command->argv[1]);
-	printf("exit\n");
-	exit(g_state.exit_status);
 }

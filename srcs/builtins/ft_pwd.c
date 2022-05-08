145696687265431ad48344d5d314b6197b093920
Rob-Yoo <jin12243@naver.com>
2022-05-08 17:26:31 +0900

    Fix : command 구조체 삭제

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -3,20 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjupar <minjupar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jinyoo <jinyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 21:32:44 by minjupar          #+#    #+#             */
-/*   Updated: 2022/05/02 17:58:24 by minjupar         ###   ########.fr       */
+/*   Updated: 2022/05/08 17:20:52 by jinyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_pwd(t_cmd *command)
+void	ft_pwd()
 {
 	char	*cur_pwd;
 
-	(void)command;
 	cur_pwd = getcwd(0, _PC_PATH_MAX);
 	printf("%s\n", cur_pwd);
 	free(cur_pwd);
